9db9b31fbeedf9c978261612db1274035d254f43
JeoHrdz <A01749164@itesm.mx>
2021-03-26 13:45:38 -0600

    EscenaMenu
    Se agrego escena menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+ * Controla los metodos para atender los componentes del menú
+ * Autores: 
+ *          Eduardo Acosta Hernández
+ *          Eric Alexis Castañeda Bravo
+ *          Jeovani Hernández Bastida
+ *          Jose Benjamín Ruíz García          
+ */
+
+public class Menu : MonoBehaviour
+{
+
+    public void Salir()
+    {
+        // Regresa al Sistema Operativo
+        Application.Quit();
+    }
+
+    public void IniciarJuego()
+    {
+        // Cambiar escena
+        //print(message:"Click al boton");
+
+        // Cambiar escena
+        SceneManager.LoadScene("EscenaJuego");
+    }
+}
